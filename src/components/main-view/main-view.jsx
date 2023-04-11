116305dbce78eebae08d4608c86978435d46fa62
jonathankerth <jonathanpkerth@gmail.com>
2023-04-11 13:48:04 -0700

    fix movie view rendering problem

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,43 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  
-  useEffect(() => {
-    fetch("https://niccage.herokuapp.com/movies")
-      .then((response) => response.json())
-      .then((data) => {
-        setMovies(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching movies:', error);
-      });
-  }, []);
-  
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
+	const [movies, setMovies] = useState([]);
+	const [selectedMovie, setSelectedMovie] = useState(null);
 
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
+	useEffect(() => {
+		fetch("https://niccage.herokuapp.com/movies")
+			.then((response) => response.json())
+			.then((data) => {
+				setMovies(data);
+			})
+			.catch((error) => {
+				console.error("Error fetching movies:", error);
+			});
+	}, []);
 
-  return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie._id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
-  );
+	if (selectedMovie) {
+		return (
+			<MovieView
+				movie={selectedMovie}
+				onBackClick={() => setSelectedMovie(null)}
+			/>
+		);
+	}
+
+	if (movies.length === 0) {
+		return <div>The list is empty!</div>;
+	}
+
+	return (
+		<div>
+			{movies.map((movie) => (
+				<MovieCard
+					key={movie._id}
+					movie={movie}
+					onMovieClick={(newSelectedMovie) => {
+						setSelectedMovie(newSelectedMovie);
+					}}
+				/>
+			))}
+		</div>
+	);
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,26 @@
 import PropTypes from "prop-types";
 
-// The MovieCard function component 
+// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
-  return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
-  );
+	return (
+		<div
+			onClick={() => {
+				onMovieClick(movie);
+			}}
+		>
+			{movie.title}
+		</div>
+	);
 };
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    director: PropTypes.string,
-    genre: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    })
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+	movie: PropTypes.shape({
+		title: PropTypes.string.isRequired,
+		image: PropTypes.string,
+		director: PropTypes.string,
+		genre: PropTypes.shape({
+			name: PropTypes.string.isRequired,
+		}),
+	}).isRequired,
+	onMovieClick: PropTypes.func.isRequired,
 };
-
